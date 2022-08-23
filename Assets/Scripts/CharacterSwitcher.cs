a6f09e8d40e5404c734adc680db94f7cd241bee4
uberBuilder24 <70218688+uberBuilder24@users.noreply.github.com>
2022-08-22 21:09:02 -0400

    Added Working Colliders

--- Assets/Scripts/CharacterSwitcher.cs
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -2,19 +2,23 @@ using UnityEngine;
 using Cinemachine;
 
 public class CharacterSwitcher : MonoBehaviour {
-    [SerializeField] private Transform person;
-    [SerializeField] private Transform dog;
+    [SerializeField] private GameObject person;
+    [SerializeField] private GameObject dog;
     [SerializeField] private CinemachineVirtualCamera virtualCam;
     public static bool personEnabled = false;
 
     void Update() {
         if (Input.GetKeyDown(KeyCode.Q)) {
-            personEnabled = !personEnabled;
-            if (personEnabled == true) {
-                virtualCam.m_Follow = person;
+            if (!personEnabled == true) {
+                virtualCam.m_Follow = person.transform;
+                person.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+                dog.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
             } else {
-                virtualCam.m_Follow = dog;
+                virtualCam.m_Follow = dog.transform;
+                person.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic;
+                dog.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
             }
+            personEnabled = !personEnabled;
         }
     }
 }
