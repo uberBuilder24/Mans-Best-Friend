600f49f06b59660cd9de16479b208715d6f7404e
uberBuilder24 <70218688+uberBuilder24@users.noreply.github.com>
2022-08-25 13:43:01 -0400

    Fixed Player Head on Wall

--- Assets/Scripts/CharacterSwitcher.cs
+++ Assets/Scripts/CharacterSwitcher.cs
@@ -4,8 +4,6 @@ using Cinemachine;
 public class CharacterSwitcher : MonoBehaviour {
     [SerializeField] private GameObject person;
     [SerializeField] private GameObject dog;
-    [SerializeField] private BoxCollider2D personHeadCollider;
-    [SerializeField] private BoxCollider2D dogHeadCollider;
     [SerializeField] private CinemachineVirtualCamera virtualCam;
     public static bool personEnabled = false;
 
@@ -15,14 +13,10 @@ public class CharacterSwitcher : MonoBehaviour {
                 virtualCam.m_Follow = person.transform;
                 person.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic; // Set person to dynamic to allow collisions
                 dog.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic; // Set dog to kinematic to prevent pushing
-                personHeadCollider.enabled = true; // Enable the person's head collider so he can naturally walk around bushes
-                dogHeadCollider.enabled = false; // Disable the dog's head collider to prevent the dog running over his face
             } else {
                 virtualCam.m_Follow = dog.transform;
                 person.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Kinematic; // Set person to kinematic to prevent pushing
                 dog.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic; // Set dog to dynamic to allow collisions
-                personHeadCollider.enabled = false; // Disable the person's head collider to prevent the dog running over his face
-                dogHeadCollider.enabled = true; // Enable the dog's head collider so he can naturally walk around bushes
             }
             personEnabled = !personEnabled;
         }
